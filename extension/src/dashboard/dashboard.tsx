d04e498a6d3d57bed5fb15a9e925d9d9846decba
Dylan Munyard <dylanmunyard@sixpivot.com.au>
2022-12-09 14:00:03 +1000

    Revert "Revert "Sample API call""
    This reverts commit 0fc129155f359d6d63a7dfea148c63be52a84b4e.

--- extension/src/dashboard/dashboard.tsx
+++ extension/src/dashboard/dashboard.tsx
@@ -1,8 +1,11 @@
+ /// <reference types="vss-web-extension-sdk" />
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as SDK from "azure-devops-extension-sdk";
 import { Header } from "azure-devops-ui/Header";
 import { Page } from "azure-devops-ui/Page";
+import { getClient } from "azure-devops-extension-api";
+import { CoreRestClient } from "azure-devops-extension-api/Core";
 
 class Dashboard extends React.Component<{}> {
   constructor(props: {}) {
@@ -12,6 +15,8 @@ class Dashboard extends React.Component<{}> {
 
   public componentDidMount() {
     SDK.init();
+    
+    const builds = getClient(CoreRestClient).getProject("ReleaseDashboard").then(console.log);
   }
 
   public render(): JSX.Element {
