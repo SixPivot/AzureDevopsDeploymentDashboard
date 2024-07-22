b1ea4b733644a24522a59d1caf1a29668a4bd18a
Jack Sowter <jhsowter@gmail.com>
2024-07-22 15:18:15 +1000

    prettier

--- extension/src/dashboard/telemetry/applicationInsights.ts
+++ extension/src/dashboard/telemetry/applicationInsights.ts
@@ -1,16 +1,16 @@
-import { ApplicationInsights } from "@microsoft/applicationinsights-web";
-import { Config } from "../config";
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+import { Config } from '../config'
 
 export const InitTelemetry = () => {
-  try {
-    const appInsights = new ApplicationInsights({
-      config: {
-        connectionString: Config.applicationInsightsConnectionString,
-      },
-    });
-    appInsights.loadAppInsights();
-  } catch (ex) {
-    console.error(ex)
-    console.warn('Could not initialize application insights')
-  }
-};
+    try {
+        const appInsights = new ApplicationInsights({
+            config: {
+                connectionString: Config.applicationInsightsConnectionString,
+            },
+        })
+        appInsights.loadAppInsights()
+    } catch (ex) {
+        console.error(ex)
+        console.warn('Could not initialize application insights')
+    }
+}
