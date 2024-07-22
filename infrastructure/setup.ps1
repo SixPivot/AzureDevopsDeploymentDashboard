116fd714baf5b09fb327fb3eaa43bcd7fd7884b3
Jack Sowter <jhsowter@gmail.com>
2024-07-22 15:16:03 +1000

    Add step to replace AI connection string in bundle.

--- infrastructure/setup.ps1
+++
@@ -1,3 +0,0 @@
-az group create --name "azure-devops-dashboard-rg" --location "australia east"
-
-az monitor app-insights component create --app "azure-devops-dashboard-ai" --location "australia east" --resource-group "azure-devops-dashboard-rg" --application-type web
