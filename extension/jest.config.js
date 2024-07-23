fd219fb686f2ce5127e0e9ea0588c3d6e4e5a452
Jack Sowter <jhsowter@gmail.com>
2024-07-23 15:58:23 +1000

    Try using babel for js and ts-jest for typescript.

--- extension/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
-    testEnvironment: 'jsdom',
-    rootDir: 'src',
-    transform: {
-        '^.+.[jt]sx?$': ['ts-jest', {}],
-    },
-    transformIgnorePatterns: ['node_modules/(?!(azure-devops-ui)/)'],
-}
