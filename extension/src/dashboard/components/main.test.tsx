1e7fa898b28bf8e723a68f216e55eb8e8f758652
Jack Sowter <jhsowter@gmail.com>
2024-07-25 14:24:39 +1000

    Test pipelines and environments are rendered.

--- extension/src/dashboard/components/main.test.tsx
+++ extension/src/dashboard/components/main.test.tsx
@@ -13,4 +13,25 @@ test('Render and check layout', async () => {
     expect(screen.getByRole('heading')).toHaveTextContent('Deployment Dashboard')
 
     await screen.findByRole('grid')
+    expect(screen.getByRole('grid')).not.toBeUndefined()
+})
+
+test('Check all pipelines are included', async () => {
+    render(<MainContent {...(data as unknown as MainContentProps)} />)
+
+    const rows = screen.getAllByRole('row')
+    expect(rows).toHaveLength(8) // includes header
+
+    for (const pipeline of data.pipelines.value) {
+        const row = screen.getByText(pipeline.name)
+        expect(row).toBeInTheDocument()
+    }
+})
+
+test('Check all environments are included', async () => {
+    render(<MainContent {...(data as unknown as MainContentProps)} />)
+    for (const env of data.environments) {
+        const column = screen.getByText(env.name)
+        expect(column).toBeInTheDocument()
+    }
 })
